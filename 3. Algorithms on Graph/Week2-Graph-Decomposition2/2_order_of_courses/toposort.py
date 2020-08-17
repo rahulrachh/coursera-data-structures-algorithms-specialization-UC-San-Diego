9cf14da811427e5aa2c330d9a6f88fe0197936da
Rahul Rachh <rahul.rachh3005.rr@gmail.com>
2020-08-17 20:14:18 +0530

    updated toposort and dfs function

--- 3. Algorithms on Graph/Week2-Graph-Decomposition2/2_order_of_courses/toposort.py
+++ 3. Algorithms on Graph/Week2-Graph-Decomposition2/2_order_of_courses/toposort.py
@@ -1,28 +1,41 @@
-#Uses python3
-
+# Uses python3
 import sys
 
 def dfs(adj, used, order, x):
     #write your code here
-    pass
+    if len(order) != len(adj) - 1:
+        for nodes in adj[x]:
+            if not used[nodes]:
+                dfs(adj, used, order, nodes)
+
+        if (used[x] == 0) and ((len(adj[x]) == 0) or all([used[x] == 1 for x in adj[x]])):
+            order.append(x)
+            used[x] = 1
 
 
 def toposort(adj):
+    #write your code here
     used = [0] * len(adj)
     order = []
-    #write your code here
+    for vertex in range(1, len(adj)):
+        dfs(adj, used, order, vertex)
+    
     return order
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    order = toposort(adj)
-    for x in order:
-        print(x + 1, end=' ')
 
+if __name__ == '__main__':
+    i = input()
+    data = list(map(int, i.split()))
+    v, e = data
+    adj = [[] for _ in range(v+1)]
+    for _ in range(1, e+1):
+        data_new = input()
+        data_new = data_new.split()
+        x, y = int(data_new[0]), int(data_new[1])
+        adj[x].append(y)
+
+    # print(adj)
+
+    order = toposort(adj)
+    for x in order[::-1]:
+        print(x, end=' ')
