b98c52a5e4d80634daf90dbb60f94e9f9b9ece21
Rahul Rachh <rahul.rachh3005.rr@gmail.com>
2020-08-19 15:28:27 +0530

    update function bipartite

--- 3. Algorithms on Graph/Week3-Paths-in-Graphs1/2_bipartite/bipartite.py
+++ 3. Algorithms on Graph/Week3-Paths-in-Graphs1/2_bipartite/bipartite.py
@@ -1,20 +1,39 @@
 #Uses python3
-
 import sys
-import queue
 
 def bipartite(adj):
-    #write your code here
-    return -1
+    color = [None] * len(adj)
+
+    for x in range(1, len(adj)):
+        if color[x] == None: color[x] = 0
+        for nodes in adj[x]:
+            if color[nodes] == None:
+                color[nodes] = color[x] ^ 1
+
+    for x in range(1, len(adj)):
+        for nodes in adj[x]:
+            if color[nodes] == color[x]:
+                return 0
+
+
+    return 1
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(bipartite(adj))
+    i = input()
+    data = list(map(int, i.split()))
+    n, m = data
+    adj = [[] for _ in range(n+1)]
+
+    for _ in range(1, m+1):
+        data_new = input()
+        data_new = data_new.split()
+        x, y = int(data_new[0]), int(data_new[1])
+        adj[y].append(x)
+        adj[x].append(y)
+
+    path = bipartite(adj)
+    if path == 1:
+        print(1)
+    else:
+        print(0)
