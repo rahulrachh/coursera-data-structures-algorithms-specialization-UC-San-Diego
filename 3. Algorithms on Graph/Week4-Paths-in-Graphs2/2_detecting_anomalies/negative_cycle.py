64e53b5af77ae38a175610a1e1ddda0944fee153
Rahul Rachh <rahul.rachh3005.rr@gmail.com>
2020-08-23 18:11:54 +0530

    updated function negative_cycle

--- 3. Algorithms on Graph/Week4-Paths-in-Graphs2/2_detecting_anomalies/negative_cycle.py
+++ 3. Algorithms on Graph/Week4-Paths-in-Graphs2/2_detecting_anomalies/negative_cycle.py
@@ -1,23 +1,38 @@
-#Uses python3
-
-import sys
-
+# uses python3
 
 def negative_cycle(adj, cost):
-    #write your code here
-    return 0
+    dist = [-1] * len(adj)
+    dist[0] = 0
 
+    for i in range(len(adj)):
+        for u in range(len(adj)):
+            for v_ind,v in enumerate(adj[j]):
+                if dist[v] > dist[u] + cost[u][v_ind]:
+                    dist[v] = dist[u] + cost[u][v_ind]
+
+
+        if i == len(adj)-2:
+            dist_list_prev = list(dist)
+        if i == len(adj)-1:
+            dist_list_curr = list(dist)
+
+
+    if dist_list_prev == dist_list_curr:
+        return 0
+    else:
+        return 1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
+    i = input()
+    data = list(map(int, i.split()))
+    n, m = data
+    adj = [[] for _ in range(n + 1)]
+    cost = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        data_new = input()
+        data_new = data_new.split()
+        x, y, c = int(data_new[0]), int(data_new[1]), int(data_new[2])
+        adj[x].append(y)
+        cost[x].append(c)
+
     print(negative_cycle(adj, cost))
