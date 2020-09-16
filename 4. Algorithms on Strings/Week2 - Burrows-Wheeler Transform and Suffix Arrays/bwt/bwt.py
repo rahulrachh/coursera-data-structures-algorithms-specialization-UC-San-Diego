17818da2c73387b6f24bb195b8ffd25e4218d2b8
Rahul Rachh <rahul.rachh3005.rr@gmail.com>
2020-09-16 12:44:31 +0530

    update the BWT function

--- 4. Algorithms on Strings/Week2 - Burrows-Wheeler Transform and Suffix Arrays/bwt/bwt.py
+++ 4. Algorithms on Strings/Week2 - Burrows-Wheeler Transform and Suffix Arrays/bwt/bwt.py
@@ -2,8 +2,24 @@
 import sys
 
 def BWT(text):
-    return ""
+    result = ''
+    bwt_strings = []
+    bwt_strings.append(text)
+
+    for index in range(1, len(text)):
+        temp_text = text[index:] + text[:index]
+        # print('Rotating Strings', temp_text)
+        bwt_strings.append(temp_text)
+
+    bwt_strings.sort()
+
+    for string in bwt_strings:
+        result += string[-1]
+
+    return result
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
+    # print('Outside the Fn:',text)
     print(BWT(text))
+    
